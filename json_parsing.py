d2039c627a96baeaf924e63da74b15f4afc8722d
Glazkov <fannik.domaschenko@yandex.ru>
2026-06-15 10:46:48 +0300

    ДЗ 6 урока.

--- json_parsing.py
+++ json_parsing.py
@@ -1,5 +1,10 @@
+import requests
 import json
 
-sting_as_json_format = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
-obj = json.loads(sting_as_json_format)
+url = 'https://gist.githubusercontent.com/KotovVitaliy/83e4eeabdd556431374dfc70d0ba9d37/raw'
+
+response = requests.get(url)
+text = response.text.strip().strip('"')   # убираем кавычки по краям
+
+obj = json.loads(text)
 print(obj['messages'][1]['message'])
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+response = requests.get("https://playground.learnqa.ru/api/long_redirect")
+
+redirects_count = len(response.history)
+final_url = response.url
+
+print("Количество редиректов:", redirects_count)
+print("Конечный URL:", final_url)
