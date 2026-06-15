ff2d90a61e214326de23198b34cfb45ebb5ed427
Glazkov <fannik.domaschenko@yandex.ru>
2026-06-15 10:34:59 +0300

    ДЗ 5 урока.

---
+++ json_parsing.py
@@ -0,0 +1,5 @@
+import json
+
+sting_as_json_format = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+obj = json.loads(sting_as_json_format)
+print(obj['messages'][1]['message'])
