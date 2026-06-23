fe4b9e16839974a9effc6ec2c7989534e456bec3
Glazkov <fannik.domaschenko@yandex.ru>
2026-06-23 12:27:00 +0300

    Дз 11 урока

--- my_test.py
+++ my_test.py
@@ -3,7 +3,7 @@ import requests
 def test_homework_cookie():
     response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
 
-    # разведка: смотрим, что прислал сервер
+    # разведка: смотрим, что прислал сервер нам
     for cookie in response.cookies:
         print(cookie.name, "=", cookie.value)
 
