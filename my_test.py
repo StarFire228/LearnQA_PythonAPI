03bdf39569b7ebb43065b633cfd4eb369cd8b060
Glazkov <fannik.domaschenko@yandex.ru>
2026-06-23 12:23:40 +0300

    Дз 10 урока

---
+++ my_test.py
@@ -0,0 +1,14 @@
+import requests
+
+def test_homework_cookie():
+    response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
+
+    # разведка: смотрим, что прислал сервер
+    for cookie in response.cookies:
+        print(cookie.name, "=", cookie.value)
+
+    # фиксируем поведение
+    assert "HomeWork" in response.cookies, \
+        "В ответе нет ожидаемой cookie"
+    assert response.cookies["HomeWork"] == "hw_value", \
+        f"Значение cookie не совпадает: {response.cookies['HomeWork']}"
